dcbff3bfdb8f0b46f02532703eea1a1c204b0d1a
victorli-cn <91169060+victorli-cn@users.noreply.github.com>
2023-05-18 15:35:32 +0800

    Update 更新触发规则

--- 补https.js
+++ 补https.js
@@ -4,7 +4,7 @@
  * @name 补https
  * @version 1.0.5
  * @description 补https
- * @rule ^([a-z.A-Z0-9-/?=]*)$
+ * @rule ^([:a-z.A-Z0-9-/? =]*)$
  * @admin false
  * @public false
  * @priority 99
@@ -13,7 +13,22 @@
 
 module.exports = async s => {
 	let http = s.param(1)
-	let https = "https://" + http
-	await s.inlineSugar(https)
+	let http_str = http.split(/[\t\r\f\n\s]*/g).join('')
+	console.log("http_str:"+http_str)
+	if(http_str.indexOf('https://') != -1) {
+		await s.inlineSugar(http_str)
+	}
+	else if(http_str.indexOf('://') != -1){
+		let https = "https" + http_str
+		await s.inlineSugar(https)
+	}
+	else if(http_str.indexOf('//') != -1){
+		let https = "https:" + http_str
+		await s.inlineSugar(https)
+	}	
+	else {
+		let https = "https://" + http_str
+		await s.inlineSugar(https)
+	}	
 	return 'next'
 }
