b31281953c03f2da2e204e320e260e4ef421ffa2
victorli-cn <91169060+victorli-cn@users.noreply.github.com>
2023-06-07 15:37:56 +0800

    Update 更新匹配规则，减少无用匹配

--- 补https.js
+++ 补https.js
@@ -4,7 +4,7 @@
  * @name 补https
  * @version 1.0.5
  * @description 补https
- * @rule ^([A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\*\+,%;\= ]*)$
+ * @rule ^([A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\*\+,%;\= ]{20, 500})$
  * @admin false
  * @public false
  * @priority 99
