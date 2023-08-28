2bbd1438df0409ad39979df1da18ab67c33499cb
victorli-cn <91169060+victorli-cn@users.noreply.github.com>
2023-08-28 16:26:55 +0800

    更新接口

--- 疯狂星期四.js
+++ 疯狂星期四.js
@@ -6,37 +6,43 @@
  *  @description lsp
  *  @rule ^(疯狂星期四)$
  *  @admin false
- *  @cron 0 30 10 * * *
  *  @public false
  *  @priority 99999
  *  @disable false
+ *  @cron 16 3 10 * * *
  */
 
 module.exports = async s => {
+	//填写定时要发送的平台
+	var platform = 'HumanTG'
+	//填写定时要发送的群id【群id和用户id必填一个，否则周四不推送】
+	var groupid=''
+	//填写定时要发送的用户id
+	var userid=''
 	s.delMsg(s.getMsgId(), { wait: 1})
 	const axios = require('axios').default
-	let week_url = "https://api.f4team.cn/API/huang/api.php"
+	let week_url = "https://www.mxnzp.com/api/holiday/single/20181121?ignoreHoliday=false&app_id=cwclhnmhw2vgojdo&app_secret=WkhnOXhKMlhOaFo5WndsNE5QUTRRdz09"
 	let week_res = await axios.request({
 		url: week_url,
 		json: true
 	})
-	let weekday_rule = /星期[\s\S]/g
-	let weekday = week_res.data.text.match(weekday_rule)
-	console.log(weekday)
-	let num_max = weekday[0].replace("星期", "")
+	//let weekday_rule = /星期[\s\S]/g
+	//let weekday = week_res.data.text.match(weekday_rule)
+	console.log(week_res.data)
+	let num_max = week_res.data.data.weekDay
 	console.log(num_max)
-	let url = "https://api.a20safe.com/api.php?api=7&key=7e361498f6839db596c34a84392b7461"
+	let url = "https://kfc-crazy-thursday.vercel.app/api/index"
 	let res = await axios.request({
-		url: url,
-		json: true
+		url: url
 	})
-	let result = res.data.data[0].result
-	console.log(result)
+	let result = res.data
+	console.log(res.data)
 	if(s.getFrom() == "cron") {
-		if(num_max == "四") {
+		if(num_max == "4") {
 			sysMethod.push({
-				platform: 'HumanTG',
-				groupId: '-1001720712689',
+				platform: platform,
+				groupId: groupid,
+				userId: userid,
 				msg: result
 			})
 		}
